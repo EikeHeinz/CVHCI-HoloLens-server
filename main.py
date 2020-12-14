328f3cc8c3cfcd9cf39bfdd86bd2fbc59cbd2230
Eike Heinz <eike.w.heinz@gmail.com>
2020-12-14 14:35:13 +0100

    tmp

--- main.py
+++ main.py
@@ -4,21 +4,18 @@ import socket
 import numpy as np
 import skimage
 
+import matplotlib.pyplot as plt
+
+import torch
+import tensorflow as tf
+
 from instance_segmentation.inference import MaskRCNNInference, WEIGHTS_PATH, IMAGE_DIR
 # import sys
 
 
 def main():
-    print("Setting up Socket")
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    server_address = ('localhost', 10000)
-    print("starting socket on %s port %s" % server_address)
-    sock.bind(server_address)
-
-    # Listen for connections
-    sock.listen()
-    print("Listening for connections...")
+    print("PyTorch CUDA: ", torch.cuda.is_available())
+    print("Tensorflow version: ", tf.__version__)
 
     # initialize image variables
     image_name = os.listdir(IMAGE_DIR)[0]
@@ -30,8 +27,22 @@ def main():
     image_point = (-1, -1)
 
     # Initialize the inference model 
-    seg_inference_model = MaskRCNNInference(WEIGHTS_PATH)
+    seg_inference_model = MaskRCNNInference(weights_path=WEIGHTS_PATH)
     sample_detections = seg_inference_model.get_detections([image])[0]
+
+    plt.imshow(sample_detections['masks'][:, :, 0])
+    plt.show()
+
+    print("Setting up Socket")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('localhost', 10000)
+    print("starting socket on %s port %s" % server_address)
+    sock.bind(server_address)
+
+    # Listen for connections
+    sock.listen()
+    print("Listening for connections...")
 
     while True:
         # Wait for connection
@@ -48,10 +59,16 @@ def main():
                     print("no more data received")
                     # This is entrypoint for model pipeline
                     # TODO how to know that all relevant data is present? we need image, depth_image and image_point
+                    # decode message: i bytes d bytes p bytes
                     break
         finally:
             connection.close()
 
+
+def decode_message(message):
+    # message is string containing identifiers for each part image (i), depth (d) and point (p)
+    # 
+    print("Not yet implemented")
 
 def estimate_shape(cropped_image):
     # TODO call shape estimation
